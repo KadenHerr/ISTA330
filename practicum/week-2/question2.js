225a7aefd806f25bb5d0c676425c930943deccbb
KadenHerr <kherr@email.arizona.edu>
2021-01-25 14:41:52 -0700

    question2 first draft

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -9,7 +9,33 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
          and therfore each of the three kids will get 8 cookies.
  */
 
- var canGetEqualCookies = function(cookies, extraCookies) {
-     
-    
+var canGetEqualCookies = function(cookies, extraCookies) {
+     cookies.reverse();
+     let numToRemove = 0;
+
+     // If there is only 1 kid or less, return true.
+     if(cookies.length <= 1) {
+        return true;
+     }
+
+     // Distribute the cookies.
+     for(var i = 1; i < cookies.length; i++) {
+        numToRemove = cookies[0] - cookies[i];
+        extraCookies -= numToRemove;
+     }
+
+     // If the extra cookies is a negative value, then there was not enough
+     // extra cookies for everyone to get an equal share, thus return false.
+     if(extraCookies < 0) {
+        return false;
+     }
+
+     // If the remaining cookies equals 0 or can be devided equally between
+     // the kids, return true.
+     if((extraCookies % cookies.length) == 0) {
+        return true;
+     }
+
+     return false;
+   
  };
