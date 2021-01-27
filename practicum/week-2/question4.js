2b4b77ed45f17f8935c34ecc32f3d756417ccb15
KadenHerr <kherr@email.arizona.edu>
2021-01-26 23:54:39 -0700

    question4 first draft

--- practicum/week-2/question4.js
+++ practicum/week-2/question4.js
@@ -11,5 +11,19 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
 */
 
 var howManyCommon = function(A, B) {
+    let count = 0;
 
+    let i = 0;
+    let j = 0;
+    let a = A.split("");
+    let b = B.split("");
+
+    for(;i<a.length;i++) {
+        for(j = 0;j<b.length;j++) {
+            if(a[i] == b[j]) {
+                count++;
+            }
+        }
+    }
+    return count;
 };
