f6f954b05c305637ecd2a2b1a4c428198edfeb56
KadenHerr <kherr@email.arizona.edu>
2021-01-27 17:11:14 -0700

    Question6 first draft

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -7,4 +7,11 @@ output: 3
 
 var median = function(input) {
 
+    input.sort;
+
+    if((input.length % 2) == 0) {
+        return (input[input.length / 2] + input[Math.floor(input.length / 2)]) / 2;
+    } else {
+        return input[Math.floor(input.length / 2)];
+    }
 };
