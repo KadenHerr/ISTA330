c9a3951077498a261a2b33b2eda9ac749c06dc40
KadenHerr <kherr@email.arizona.edu>
2021-01-27 23:34:01 -0700

    Question 12 first draft

--- practicum/week-2/question12.js
+++ practicum/week-2/question12.js
@@ -16,4 +16,46 @@ output: 4 because the following partition has the highest number of balanced sub
 
 var maxBalanceNumber = function(input) {
 
+  let count1 = 0;
+  let count2 = 0;
+  let i = 0;
+  let j = 0;
+  let retVal = 0;
+
+  while(j < input.length) {
+
+      if(input[i] == input[j]) {
+      count1++;
+      j++;
+      continue;
+    }
+
+    i = j;
+    while(count2 < count1) {
+
+      if(j >= input.length) {
+        return retVal + 1;
+      }
+
+      if(input[i] == input[j]) {
+        count2++;
+        j++;
+
+        if(count1 == count2) {
+          retVal++;
+          i = j;
+          count1 = 0;
+          count2 = 0;
+        }
+
+      } else {
+        retVal++;
+        i = j;
+        count1 = 0;
+        count2 = 0;
+      }
+    }
+  }
+
+  return retVal;
 };
