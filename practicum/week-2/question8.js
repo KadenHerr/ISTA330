858a154bd603b454c711ecb2a3f583f5cefebd15
KadenHerr <kherr@email.arizona.edu>
2021-01-27 18:04:17 -0700

    question 8 first draft

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -8,5 +8,26 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
+    let count = 0;
+    retVal = [];
 
+    for(var i = 0; i < input.length;i++) {
+        count = 0;
+        for(var j = 0; j < input.length;j++) {
+
+            // Do not compare the same elements
+            if(i == j) {
+                continue;
+            }
+
+            // If the conditions are met, increment the counter
+            if(input[i]<input[j] && (input[j] % 2) == 0) {
+                count++;
+            }
+        }
+        // Store the counter in the return array
+        retVal.push(count);
+    }
+
+    return retVal;
 };
