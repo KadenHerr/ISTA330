3ad00c450a0a9e2c3ba62b209386f360bca00923
KadenHerr <kherr@email.arizona.edu>
2021-01-27 17:47:28 -0700

    Q7 first draft

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -12,4 +12,13 @@ output: 4
 
 var maxSumOfMins = function(input) {
 
+       let maxSum = 0;
+
+       input.sort(function(a,b){return a - b});
+
+       for(var i = 0; i < input.length; i += 2) {
+              maxSum += input[i];
+       }
+
+       return maxSum;
 };
