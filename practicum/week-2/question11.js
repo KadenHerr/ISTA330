bf25ceecdb5cb9c226a77e3c42209e5a8b73888d
KadenHerr <kherr@email.arizona.edu>
2021-01-27 18:59:29 -0700

    Question 11 first draft

--- practicum/week-2/question11.js
+++ practicum/week-2/question11.js
@@ -11,4 +11,13 @@ output: [5, 5, 2, 2, 2, 2]
 
 var decode = function(input) {
 
+    let retVal = [];
+
+    for(var i = 0; i < input.length; i += 2) {
+        for(var j = 0; j < input[i]; j++) {
+            retVal.push(input[i+1]);
+        }
+    }
+
+    return retVal;
 };
