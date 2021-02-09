21bfde40028053d436dcf2e3fed2d00fccbde801
KadenHerr <kherr@email.arizona.edu>
2021-02-09 11:44:20 -0700

    First Draft Q1

--- practicum/week-3/question1.js
+++ practicum/week-3/question1.js
@@ -10,5 +10,26 @@ is at least twice as much as every other number
 */
 
 var largestNumberIsAtLeastTwice = function(input) {
-   
+   let maxVal = Math.max(...input);
+
+   let index = input.indexOf(maxVal);
+   let otherVars = [];
+
+   // Store all of the values that are not the one max value in an array.
+   for(var i=0;i<input.length - 1;i++) {
+        if(i != index) {
+                otherVars[i] = input[i];
+        }
+   }
+
+   // Find half of the max value.
+   maxVal = maxVal / 2;
+
+   for(var i = 0; i<otherVars.length;i++) {
+        if(otherVars[i] > maxVal) {
+                return false;
+        }
+   }
+   // if the code reaches this point then all of the values are half or less.
+   return true;
 };
