80859c7485e782c6163ab661256ee750f1bc8c8d
KadenHerr <kherr@email.arizona.edu>
2021-02-10 12:25:19 -0700

    Q6 First draft

--- practicum/week-3/question6.js
+++ practicum/week-3/question6.js
@@ -12,5 +12,18 @@ output: 2
 */
 
 var m_element = function(input) {
-
+    let count = 1;
+    let goal = Math.floor(input.length/2) + 1;
+    for(let i=0; i<input.length - 1; i++) {
+        for(let j=i+1; j<input.length; j++) {
+            if(input[i] == input[j]) {
+                count++;
+            }
+        }
+        if(count >= goal) {
+            return input[i];
+        }
+        count = 1;
+    }
+    return -1;
 };
