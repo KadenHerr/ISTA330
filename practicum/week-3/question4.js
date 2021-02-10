4fb0ae8b6c9529a4fa67f942b983daccf836fdb3
KadenHerr <kherr@email.arizona.edu>
2021-02-10 10:42:42 -0700

    Q4 first draft

--- practicum/week-3/question4.js
+++ practicum/week-3/question4.js
@@ -13,5 +13,12 @@ Example:
 */
 
 var hasDuplicates = function(input) {
-
+    for(let i=0;i<input.length-1; i++) {
+        for(let j=i+1; j<input.length; j++) {
+            if(input[i] == input[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
 };
