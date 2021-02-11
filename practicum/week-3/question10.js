c557a68917a3f20e3be6547931f828dfba80f5a7
KadenHerr <kherr@email.arizona.edu>
2021-02-10 17:15:51 -0700

    Q10 First draft

--- practicum/week-3/question10.js
+++ practicum/week-3/question10.js
@@ -16,5 +16,30 @@ output: [[-5, -4], [23, 24]]
 */
 
 var minPairs = function(input) {
+    
+    let minDif = Number.MAX_VALUE;
+    let curDif;
+    let retVal = [];
+    
+    input.sort(function(a, b){return a - b});
 
+    // Find the minimum difference.
+    for(let i=1; i<input.length; i++) {
+        curDif = (Math.max(input[i],input[i-1]) - Math.min(input[i],input[i-1]));
+        if(curDif < minDif) {
+            minDif = curDif;
+        }
+    }
+
+    // Find the pairs that have this minimum difference.
+    for(let i=1; i<input.length; i++) {
+        curDif = (Math.max(input[i],input[i-1]) - Math.min(input[i],input[i-1]));
+        if(curDif == minDif) {
+            retVal.push([input[i-1],input[i]]);
+        }
+    }
+    // Return the minimum difference pairs.
+    return retVal;
 };
+let arr = minPairs([1,-5,-10,24,19,-4,-14,23]);
+console.log(arr[0]);
