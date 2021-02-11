7b466315907f5dcca80a12f901e3013ae9cd7525
KadenHerr <kherr@email.arizona.edu>
2021-02-10 18:21:00 -0700

    Q11 first draft

--- practicum/week-3/question11.js
+++ practicum/week-3/question11.js
@@ -14,5 +14,21 @@ output: 1
 */
 
 var d_count = function(input1, input2, d) {
+    let dCount = 0;
+    let isD = true;
 
+    for(let i=0; i<input1.length; i++) {
+        for(let j=0; j<input2.length; j++) {
+            if(Math.abs(input1[i] - input2[j]) <= d) {
+                isD = false;
+                break;
+            }
+        }
+        if(isD) {
+            dCount++;
+        }
+        // Reset isD
+        isD = true;
+    }
+    return dCount;
 };
